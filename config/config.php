d1272bd83cfb34661d3d380e8e70ad369926777c
yunrileo <111698317+yunrileo@users.noreply.github.com>
2022-08-21 23:57:24 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('BASE_PATH', dirname(dirname(__FILE__)));
+define('APP', BASE_PATH.'\app');
+define('LIBRARY', BASE_PATH.'\library');
+define('PUBLIC', BASE_PATH.'\public');
+define('CONTROLLER', APP.'\Controller');
+define('MODEL', APP.'\Model');
+define('VIEW', APP.'\Views');
+
+
